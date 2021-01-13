7545f850c794ee5a68ccf4a410d34bef52bf85cf
Brian Firestone <bfirestone2339@gmail.com>
2021-01-12 21:58:01 -0500

    commit tree 8fbdeed6a2aa096cd9b55a9a449ec95ae4ae2083

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,21 +1,34 @@
 def selects_all_female_bears_return_name_and_age
-  "SELECT name, age FROM bears WHERE sex = 'F';"
+  "SELECT name, age 
+  FROM bears 
+  WHERE sex = 'F';"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
-  "SELECT name FROM bears ORDER BY name;"
+  "SELECT name 
+  FROM bears 
+  ORDER BY name;"
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "SELECT name, age FROM bears WHERE alive = 1 ORDER BY age ASC;"
+  "SELECT name, age 
+  FROM bears 
+  WHERE alive = 1 
+  ORDER BY age ASC;"
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "SELECT name, age FROM bears ORDER BY age DESC LIMIT 1;"
+  "SELECT name, age 
+  FROM bears 
+  ORDER BY age DESC 
+  LIMIT 1;"
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "SELECT name, age FROM bears ORDER BY age ASC LIMIT 1;"
+  "SELECT name, age 
+  FROM bears 
+  ORDER BY age ASC 
+  LIMIT 1;"
 end
 
 def selects_most_prominent_color_and_returns_with_count
@@ -27,9 +40,14 @@ def selects_most_prominent_color_and_returns_with_count
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "SELECT COUNT(temperament) FROM bears GROUP BY temperament HAVING temperament = 'goofy';"
+  "SELECT COUNT(temperament) 
+  FROM bears 
+  GROUP BY temperament 
+  HAVING temperament = 'goofy';"
 end
 
 def selects_bear_that_killed_Tim
-  "SELECT * FROM bears WHERE name IS NULL;"
+  "SELECT * 
+  FROM bears 
+  WHERE name IS NULL;"
 end
